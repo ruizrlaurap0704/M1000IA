2b22f805b722866bef944d2ce2003d248bff5cf5
ruizrlaurap0704 <laurapaola7@gmail.com>
2024-06-25 22:30:36 -0300

    segundo commit

--- FireDetectorAPI-master/main.py
+++ FireDetectorAPI-master/main.py
@@ -12,8 +12,10 @@ from flask import jsonify
 origins = [
 "http://localhost:8000",
 "http://localhost:5500",
+"http://localhost:5502",
 "http://127.0.0.1:8000",
-"http://127.0.0.1:5500"
+"http://127.0.0.1:5500",
+"http://127.0.0.1:5502"
 ]
 
 app = FastAPI()
